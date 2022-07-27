2c5e711179168834680f558fbc48ae52040712fe
guyam23-meet <guyamibar@gmail.com>
2022-07-27 17:00:57 +0300

    i did the thing guys

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -6,24 +6,31 @@ app = Flask(  # Create a flask app
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
-app.config['SECRET_KEY'] = 'super-secret-key'
+#sapp.config['SECRET_KEY'] = 'super-secret-key'
 
 
 username = "(╯°□°）╯︵ ┻━┻"
-password = "):<"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+password = "flip"
+facebook_friends=["Never","Gonna","Give", "You", "Up"]
 
 
 @app.route('/',methods=["GET","POST"])  # '/' for the default page
 def login():
 	if request.method=="POST":
-		if request.form['username']==username && request.form['password']==password:
-			return render_template('home.html')
+		if request.form['username']==username and request.form['password']==password:
+			return redirect(url_for('home'))
+		else:
+			return render_template('login.html')
 	else:
 		return render_template('login.html')
-  
 
+@app.route('/home')
+def home():
+	return render_template('home.html',fb=facebook_friends)
 
+@app.route('/friend_exists/<string:name>',methods=["GET","POST"])
+def friend_exist(name):
+	return render_template('friend_exists.html',name=name,fb=facebook_friends)
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
